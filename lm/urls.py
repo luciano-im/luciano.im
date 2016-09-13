5ef7d186c7fc2e0f6756a988b0cd6d7cb21bb382
Luciano Muñoz <luchisds@gmail.com>
2016-09-13 13:32:21 -0300

    del admin url & add new work

--- lm/urls.py
+++ lm/urls.py
@@ -23,6 +23,6 @@ from website import views
 urlpatterns = [
     #static.serve debe ser usado solo en dev environment Fuck!
     url(r'^static/(?P<path>.*)$','django.views.static.serve',{'document_root': settings.STATIC_ROOT}),
-    url(r'^admin/', admin.site.urls),
+    #url(r'^admin/', admin.site.urls),
     url(r'^$', views.IndexView.as_view(), name='home'),
 ]
